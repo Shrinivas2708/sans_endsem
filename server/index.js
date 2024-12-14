f5323b261ef5da0825927e0135d70942522f3a7d
Shrinivas2708 <ssherikar2005@gmail.com>
2024-12-14 18:07:27 +0530

    hehe

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const multer = require("multer");
+
 const path = require("path");
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({ message: "server is running 8090" });
